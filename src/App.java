4874eb76713a30e4ef4f95e187cd3ae4f97c1b96
mshirazkamran <blogger31625@gmail.com>
2024-12-27 15:26:55 +0500

    changed App.java main method

--- src/App.java
+++ src/App.java
@@ -7,17 +7,6 @@ import logic.subjects.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        // new CodingSubject().openCodeEditor();
-        //runs
-        // CommandBuilder.runCommand("start chrome G:\\IMP-FILES\\Semester-3\\Software-Engineering\\3-Lectures\\SEC-FALL-2023-Lecture#1.pdf"); // Opens Visual Studio Code
-
-    //    Subject sub = new Subject("SEC");
-    //    sub.openFilesInRange(3,7);
-
-        SwingUtilities.invokeLater( () -> {
-            GUIbuilder.buildGUI(new Student("shiraz"));
-        });
-        // GUIbuilder.buildGUI(new Student("shiraz"));
+        GUIbuilder.buildGUI(new Student("shiraz"));
     }
 }
